cb402c30bc5b29b05e4794b6d3eb0b7c3d8f29d8
Anderson Calixto <andersonbr@gmail.com>
2017-05-23 01:09:08 -0300

    inicio servico

---
+++ src/main/java/br/com/shellcode/artigo/rs/OAuthProvider.java
@@ -0,0 +1,10 @@
+package br.com.shellcode.artigo.rs;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Path;
+
+@RequestScoped
+@Path("/oauth")
+public class OAuthProvider {
+
+}
